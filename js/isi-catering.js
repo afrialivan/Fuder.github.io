01484021441a1612f5b7033d8b28c669a7754f70
afrialivan <afrialivan0@gmail.com>
2023-01-16 09:31:10 +0800

    update

---
+++ js/isi-catering.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i < pilih.length; i++) {
+//   pilih[i].style.backgroundColor = "red";
+// }
+
+const oi = document.getElementById('card')
+const pilih = document.getElementById('pilihan')
+
+function pilih(pilih) {
+  pilih.style.backgroundColor='black'
+}
