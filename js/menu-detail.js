d9149e1ddb8aeb7aace8e23baa7b4027fc6b043f
daffah27 <muh.daffah27@gmail.com>
2023-02-07 07:38:41 +0800

    oi

---
+++ js/menu-detail.js
@@ -0,0 +1,7 @@
+const bg = document.getElementById('alerts')
+// document.getElementById('keranjang').onclick() = aler()
+
+document.getElementById('keranjang').addEventListener("click", pengetahuan)
+function pengetahuan(){
+    bg.style.backgroundColor='black'
+}
