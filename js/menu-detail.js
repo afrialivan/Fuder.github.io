600447f1d94d50634ffb419f95b7e998d6c70652
daffah27 <muh.daffah27@gmail.com>
2023-02-07 09:39:11 +0800

    update menu

--- js/menu-detail.js
+++ js/menu-detail.js
@@ -1,7 +1,21 @@
-const bg = document.getElementById('alerts')
-// document.getElementById('keranjang').onclick() = aler()
+// const bg = document.getElementById('alerts')
+// // document.getElementById('keranjang').onclick() = aler()
 
-document.getElementById('keranjang').addEventListener("click", pengetahuan)
-function pengetahuan(){
-    bg.style.backgroundColor='black'
+// // const keranjang = document.getElementById('keranjang1')
+
+
+// // document.querySelector('body').onclick() = alert("test")
+// document.querySelector('keranjang').addEventListener('click', alert('oi'))
+
+// keranjang.onclick = function(){
+//     bg.style.backgroundColor="black"
+// }
+
+// // function oi(){
+// //     bg.style.backgroundColor="black"
+// // }
+
+function keranjang(){
+    document.getElementById("alerts").classList.add("ada")
+    console.log("hi")
 }
