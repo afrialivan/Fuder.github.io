d097dc6599c8182ea121e12011f0128fd2c5f36f
afrialivan <afrialivan0@gmail.com>
2023-02-12 13:30:04 +0800

    updte

---
+++ js/keranjang.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('pengataran')
+
+btn.onclick = function pengataran(){
+    document.querySelector(".notiff").classList.add("notif-aktif")
+    console.log("hi")
+}
